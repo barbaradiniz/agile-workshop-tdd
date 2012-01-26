357bed5d48982c9f1c4253dfb6ffbc37c69a1466
barbara <bdsp.pe@gmail.com>
2012-01-26 11:59:59 -0200

    Correcao do exercicio 3

--- src/br/com/caelum/tdd/exercicio3/ProcessadorDeBoletos.java
+++ src/br/com/caelum/tdd/exercicio3/ProcessadorDeBoletos.java
@@ -5,9 +5,15 @@ import java.util.List;
 public class ProcessadorDeBoletos {
 
 	public void processa(List<Boleto> boletos, Fatura fatura) {
+		double valorPago = 0.0;
 		for(Boleto boleto : boletos) {
 			Pagamento pagamento = new Pagamento(boleto.getValor(), MeioDePagamento.BOLETO);
 			fatura.getPagamentos().add(pagamento);
+			valorPago += boleto.getValor();
+		}
+		
+		if (valorPago >= fatura.getValor()) {
+			fatura.setPago(true);
 		}
 	}
 }
